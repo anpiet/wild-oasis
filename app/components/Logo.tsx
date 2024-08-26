9e9d1e615b35184c32a4747ed5b7e69a86df04fc
Andrzej Pietkiewicz <pietan@me.com>
2024-08-26 22:22:59 +0200

    Initial commit

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -6,7 +6,7 @@ function Logo() {
   return (
     <Link href='/' className='flex items-center gap-4 z-10'>
       <Image
-        src='/icon.png'
+        src={logo}
         height='60'
         quality={100}
         width='60'
